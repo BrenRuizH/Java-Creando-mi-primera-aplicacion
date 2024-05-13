c14c2f14fcb39edb5a2c285aa0b8cb67923606ae
Brenda Ruiz <ruizhernandezbrendac@gmail.com>
2024-05-13 13:40:00 -0600

    2024-05-13 Tomando decisiones #1

---
+++ src/Decisiones.java
@@ -0,0 +1,15 @@
+public class Decisiones {
+
+    public static void main(String[] args) {
+
+        int fechaLanzamiento = 1999;
+        boolean incluidoPlan = true;
+        double notaPelicula = 8.2;
+
+        if(fechaLanzamiento >= 2022) {
+            System.out.println("Películas más populares");
+        } else {
+            System.out.println("Películas retro que aún vale la pena ver");
+        }
+    }
+}
